0e3292e262016c892ecbc717afdfc111ee5c77f4
Marisa-Salazar <salazar.marisa15@gmail.com>
2023-11-24 22:31:58 -0700

    added Dictionary component with search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <h1>Dictionary Project </h1>
+      <Dictionary />
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function Search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={Search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
