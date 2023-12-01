a86ed0e2d6c5fc670aff54007ab91cf9fcb9e0b3
Marisa-Salazar <salazar.marisa15@gmail.com>
2023-11-30 21:02:34 -0700

    added background colors

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ function App() {
   return (
     <div className="App">
       <Dictionary defaultKeyword="Sunset" />
+      <footer>
+        This project was coded by Marisa Salazar and is open-sourced on
+        <a
+          className="App-link text-center mt-3"
+          href="https://github.com/Marisa-Salazar/Dictionary-React-App"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Github{" "}
+        </a>
+      </footer>
     </div>
   );
 }
